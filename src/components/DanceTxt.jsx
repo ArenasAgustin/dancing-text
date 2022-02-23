43e7cc2a27131e68bf96735d098fb1b78871b846
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-02-23 20:09:02 -0300

    first commit

---
+++ src/components/DanceTxt.jsx
@@ -0,0 +1,79 @@
+import "./DanceTxt.scss";
+
+export default function Home() {
+  return (
+    <div className="txts-container">
+        <div className="txt-container">
+          <p className="txt-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            sapiente officiis vel blanditiis explicabo laborum corrupti earum
+            aspernatur vitae cum, necessitatibus a cupiditate culpa nemo fugit
+            incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolores sapiente officiis vel
+            blanditiis explicabo laborum corrupti earum aspernatur vitae cum,
+            necessitatibus a cupiditate culpa nemo fugit incidunt maxime impedit
+            beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolores sapiente officiis vel blanditiis explicabo laborum corrupti
+            earum aspernatur vitae cum, necessitatibus a cupiditate culpa nemo
+            fugit incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolores sapiente officiis vel
+            blanditiis explicabo laborum corrupti earum aspernatur vitae cum,
+            necessitatibus a cupiditate culpa nemo fugit incidunt maxime impedit
+            beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolores sapiente officiis vel blanditiis explicabo laborum corrupti
+            earum aspernatur vitae cum, necessitatibus a cupiditate culpa nemo
+            fugit incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit.
+          </p>
+        </div>
+
+        <div className="txt-container">
+          <p className="txt-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            sapiente officiis vel blanditiis explicabo laborum corrupti earum
+            aspernatur vitae cum, necessitatibus a cupiditate culpa nemo fugit
+            incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolores sapiente officiis vel
+            blanditiis explicabo laborum corrupti earum aspernatur vitae cum,
+            necessitatibus a cupiditate culpa nemo fugit incidunt maxime impedit
+            beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolores sapiente officiis vel blanditiis explicabo laborum corrupti
+            earum aspernatur vitae cum, necessitatibus a cupiditate culpa nemo
+            fugit incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolores sapiente officiis vel
+            blanditiis explicabo laborum corrupti earum aspernatur vitae cum,
+            necessitatibus a cupiditate culpa nemo fugit incidunt maxime impedit
+            beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolores sapiente officiis vel blanditiis explicabo laborum corrupti
+            earum aspernatur vitae cum, necessitatibus a cupiditate culpa nemo
+            fugit incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit.
+          </p>
+        </div>
+
+        <div className="txt-container">
+          <p className="txt-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            sapiente officiis vel blanditiis explicabo laborum corrupti earum
+            aspernatur vitae cum, necessitatibus a cupiditate culpa nemo fugit
+            incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolores sapiente officiis vel
+            blanditiis explicabo laborum corrupti earum aspernatur vitae cum,
+            necessitatibus a cupiditate culpa nemo fugit incidunt maxime impedit
+            beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolores sapiente officiis vel blanditiis explicabo laborum corrupti
+            earum aspernatur vitae cum, necessitatibus a cupiditate culpa nemo
+            fugit incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Dolores sapiente officiis vel
+            blanditiis explicabo laborum corrupti earum aspernatur vitae cum,
+            necessitatibus a cupiditate culpa nemo fugit incidunt maxime impedit
+            beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolores sapiente officiis vel blanditiis explicabo laborum corrupti
+            earum aspernatur vitae cum, necessitatibus a cupiditate culpa nemo
+            fugit incidunt maxime impedit beatae.Lorem ipsum dolor sit amet
+            consectetur adipisicing elit.
+          </p>
+        </div>
+      </div>
+  );
+}
